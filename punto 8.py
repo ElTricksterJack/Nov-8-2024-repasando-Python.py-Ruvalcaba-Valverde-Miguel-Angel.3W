c859e3a486a8a0ffe197fdd656e4b60ce95639f9
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 17:21:35 -0600

    Update punto 8.py

--- punto 8.py
+++ punto 8.py
@@ -3,6 +3,6 @@ print("-----------Actividad 8-----------")
 n = int(input("Ingrese un numero entero: "))#caoturar valor.
 for f in range(1,11):#aqyu se hase un for que se repita 10 veces grasis al range
 	tap = n * f #operasion echa
-	print(f'7 x {f} = {tap}') #esti se escribe con el for mostrando la tambla
+	print(f'{n} x {f} = {tap}') #esti se escribe con el for mostrando la tambla
 print("--------------------------------------")
 print("Resultado: se be la tabla de multiplicar de cualquier numero pedido.\n")
