93e282c4ca785b5410a5459899f6e21decabed44
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 16:44:40 -0600

    Update punto 5.py
    5.Comprobar si la fruta cereza se encuentra en una lista de frutas. En caso afirmativo, imprimir
    por pantalla el texto Cereza SÍ está en la lista. En caso contrario, imprimir por
    pantalla el texto Cereza NO está en la lista.

--- punto 5.py
+++ punto 5.py
@@ -1,1 +1,15 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 5-----------")
+print("escribe una lista de frutas")#preimo una aclarasion ante que todo.
+m1 = input("Materia 1:")#despues capturamos los frutas
+m2 = input("Materia 2:")
+m3 = input("Materia 3:")
+m4 = input("Materia 4:")
+m5 = input("Materia 5:")
+lis = [m1,m2,m3,m4,m5]#hasemos la lista pedida
+if "cereza".lower() in lis:#con esto comprobamos si cereza esta o no en la lista
+    print("Cereza SI está en la lista")
+else:
+    print("Cereza NO esta en la list")
+print("--------------------------------------")
+print("Resultado: si esta cereza se ve el primer mesaje.\n")
