8b56e38268280bc92e123c79fc18d35e9576cbfa
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 16:57:00 -0600

    Update punto 6.py
    Dada una lista con nombre y dos apellidos, imprime solo los dos apellidos utilizando índices.

--- punto 6.py
+++ punto 6.py
@@ -1,1 +1,10 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 6-----------")
+name = input("di un nombre: ")#capturamos valores al igual que buen mastro pokemon
+ap1 = input("di un apellido: ")
+ap2 = input("di otro apellido: ")
+lst=[name,ap1,ap2]#que no se te olvide la lista
+print("el primer apellido es: ",lst[1])#por ultimo solo mostramos los valores pedidos en pantalla
+print("el segundo apellido es: ",lst[2])
+print("--------------------------------------")
+print("Resultado: tal y como dise la actividad se imprimern en pantalla los 2 ultimos valores de apellido en pantalla.\n")
