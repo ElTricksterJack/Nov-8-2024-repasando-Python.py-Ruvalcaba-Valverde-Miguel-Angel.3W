ef40927db6be1d6f3fc9a25282e5a676972a4619
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 16:20:48 -0600

    Update punto 3.py
    3.Existen dos variables, una con un nombre y otra con un apellido. Primero se ha de comprobar el
    nombre, si es igual a Daniel, se comprueba el apellido, si es igual a Ramos, se imprime por
    pantalla el texto Nombre y apellido correctos. En caso de que el nombre sea Daniel,
    pero el apellido no sea Ramos, se imprime por pantalla el texto Apellido incorrecto. En
    caso que el nombre no sea Daniel, se imprime por pantalla el texto Usuario desconocido.

--- punto 3.py
+++ punto 3.py
@@ -1,1 +1,14 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 3-----------")
+name = str(input("Escoje un Nombre: "))
+ap = str(input("Allade un Apellido: "))#capturar nombres
 
+if name in "Daniel".lower():#haser cadena para preguntar por el nombre
+    if ap in "Ramos".lower():#haser otra cadena de if dentro de otra, que pregunte por el apellido
+        print("Nombre y apellido correctos.")
+    else:
+        print("Apellido incorrecto.")
+else:#si en cualquiera de ambas es falso, maracara el mensaje
+    print("Usuario desconocido.")#tranquilo la actividad es muy ambigua.
+print("--------------------------------------")
+print("Resultado: Si se las estrellas se aliena para dar el apellido y nombre corepto se bera si es el ususario corepto.\n")
