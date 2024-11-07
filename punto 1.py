0c150a58abb4fe47006bec232b71280a45f58ed5
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 15:42:46 -0600

    Create punto 1.py
    1 Comprueba si una edad almacenada en una variable es mayor de edad, es decir, que su valor sea
    mayor de 18. Si es así, imprime el texto Eres mayor de edad. En caso contrario, imprime el
    texto NO eres mayor de edad.

---
+++ punto 1.py
@@ -0,0 +1,14 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 1-----------")
+e = int(input("Porfavor ingrese su edad:"))
+if e >= 18:
+    print("Eres mayor de edad")#buena suerte
+elif e >= 100:
+    print("cuantas beses a esquibado la muerte?")
+elif e <= 0:
+    print("pues aun no as nasido hasi que")
+    print("NO eres mayor de edad")
+else:
+    print("NO eres mayor de edad")
+print("--------------------------------------")
+print("Resultado:se comprueba la esdad y el posible margen de error.\n")
