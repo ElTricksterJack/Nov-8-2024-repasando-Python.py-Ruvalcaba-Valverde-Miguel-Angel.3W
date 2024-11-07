4930041fc96da7b9438975c4c89e19cc78b700d9
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 16:09:05 -0600

    Update punto 2.py
    Dada una nota almacenada en una variable, imprime su equivalente:
    • Mayor o igual a 0, pero menor que 5, SUSPENSO.
    • Mayor o igual a 5, pero menor que 6, SUFICIENTE.
    • Mayor o igual a 6, pero menor que 7, BIEN.
    • Mayor o igual a 7, pero menor que 9, NOTABLE.
    • Mayor o igual a 9, pero menor o igual a 10, SOBRESALIENTE.
    En cualquier otro caso, imprimir el texto: NOTA NO VÁLIDA.

--- punto 2.py
+++ punto 2.py
@@ -1,1 +1,23 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 2-----------")
+cal = int(input("En una escala del 1 al 10 cual es tu calificasion?:"))#capturas el valor
+SUSPENSO = cal <= 4#creas variables
+SUFICIENTE = cal >= 5
+BIEN = cal >= 7
+NOTABLE = cal >= 9
+SOBRESALIENTE = cal == 10
+if SUSPENSO:#hases una cadena de if con cada una de esas variables con un mensaje difere
+    print("estas suspendido idiota")
+elif SUFICIENTE:
+    print("suficiente pero repugnate")
+elif BIEN:
+    print("bien")
+elif NOTABLE:
+    print("esto es notable, te felisito")
+elif SOBRESALIENTE:
+    print("eres sobresaliente sige hasi")
+else:#si fallas te insuta
+    print("idiota no sabes calificar No sabe calificar")#cuando asia esto el programa relleno la respuesta despues del 
+#idiota, escribio lo demas hasta donde dicer calificar
+print("--------------------------------------")
+print("Resultado: al se calificado se disne las verdades, y los pedido de la actividad\n")
