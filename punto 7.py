4f6c3c6725f35a5d7983bb4f6484501eade57509
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 17:14:43 -0600

    Update punto 7.py

--- punto 7.py
+++ punto 7.py
@@ -1,1 +1,9 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 7-----------")
+pc = sum(range(2, 101, 2))
+#el range es una funcion que devuelve una secuencia de numeros desdeel primero hasta el ultimo.
+#en este caso hasemos que sea del 2 al 100 y limitamos a los pares
+#si se preguntan porque un 101 es para que cuente completo.
+print("La suma de todos los números pares desde 2 hasta 100 es:",pc )#se muestra la suma de los pares del 2 al 100
+print("--------------------------------------")
+print("Resultado: se muestra la suma de los pares del 2 al 100.\n")
