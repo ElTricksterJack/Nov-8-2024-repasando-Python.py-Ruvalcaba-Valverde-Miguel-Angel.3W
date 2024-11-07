fa520da4cb99c9e30bf688e79fcd9d0ed1cb1ae4
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 16:31:22 -0600

    Update punto 4.py
    4.Dada una lista con diferentes notas, se debe imprimir la más baja y la más alta.

--- punto 4.py
+++ punto 4.py
@@ -1,1 +1,18 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 4-----------")
+print("escribe tus calificasiones de las 7 materias porfavor")#preimo una aclarasion ante que todo.
+m1 = float(input("Materia 1:"))#despues capturamos los datos de las calificaciones 
+m2 = float(input("Materia 2:"))
+m3 = float(input("Materia 3:"))
+m4 = float(input("Materia 4:"))
+m5 = float(input("Materia 5:"))
+m6 = float(input("Materia 6:"))
+m7 = float(input("Materia 7:"))
+lis = [m1,m2,m3,m4,m5,m6,m7]#hasemos la lista pedida
+max = max(lis)#hasemos los valores del minimo y el maximo
+min = min(lis)
+print("el valor mas vajo es de: ", min)#y mostramos los resultados
+print("el valor mas alto es de: ", max)
+print("--------------------------------------")
+print("Resultado: se ven los valores altos y bajos.\n")
 
