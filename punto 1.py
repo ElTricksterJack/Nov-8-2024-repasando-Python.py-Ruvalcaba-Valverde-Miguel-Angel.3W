b4e0071c67a4c7702eca6be0b19e4372c6dd587b
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 15:48:26 -0600

    Update punto 1.py

--- punto 1.py
+++ punto 1.py
@@ -1,14 +1,11 @@
 print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
 print("-----------Actividad 1-----------")
 e = int(input("Porfavor ingrese su edad:"))
-if e >= 18:
+if e > 18:
     print("Eres mayor de edad")#buena suerte
-elif e >= 100:
-    print("cuantas beses a esquibado la muerte?")
-elif e <= 0:
-    print("pues aun no as nasido hasi que")
-    print("NO eres mayor de edad")
 else:
-    print("NO eres mayor de edad")
+    print("NO eres mayor de edad")#que afortunado
 print("--------------------------------------")
-print("Resultado:se comprueba la esdad y el posible margen de error.\n")
+print("Resultado:se comprueba la edad.\n")
+#funsiona de manera simple, ingresas el valor de edad y si coinside con el if , te muestra si eres mayor de edad o no.
+#balla esta herramienta nueva de mejora de texto esta chida.
