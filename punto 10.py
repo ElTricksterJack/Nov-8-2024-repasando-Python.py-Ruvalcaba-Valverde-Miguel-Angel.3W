725fdf1c703c312211ade999dec4f7a032a10f3e
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 20:01:30 -0600

    Update punto 10.py

--- punto 10.py
+++ punto 10.py
@@ -1,1 +1,16 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 10-----------")
+print("qui escribe 5 animes")#como dije hay que haser una aclarasion
+m1 = input("Anime 1:")#ahora bamos a capturar los datos
+m2 = input("Anime 2:")
+m3 = input("Anime 3:")
+m4 = input("Anime 4:")
+m5 = input("Anime 5:")
+anim=[m1,m2,m3,m4,m5]#los juntamos en una lista
+print("----------------")
+i = 0#se declara una variable
+while i < len(anim):#y con el while se hase un bucleO
+  print(anim[i])
+  i = i + 1
+print("--------------------------------------")
+print("Resultado: lo mismo que el otro pero con un while y mas copmlejo.\n")
