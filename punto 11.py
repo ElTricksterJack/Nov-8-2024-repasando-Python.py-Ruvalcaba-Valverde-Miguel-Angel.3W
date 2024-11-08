90bc6413d1ced183cef2bb9c724897ae4b86167d
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 20:10:32 -0600

    Update punto 11.py

--- punto 11.py
+++ punto 11.py
@@ -1,1 +1,8 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 11-----------")
+m1 = input("escribe lo que quieras, palabra o oracion:")#escribes la palabra o oracion
+print("----------------")
+for x in m1:#y el bucle se encarga de los demas
+  print(x)
+print("--------------------------------------")
+print("Resultado: lo mismo que las otras 2 pero con for y una cadena de texto.\n")
