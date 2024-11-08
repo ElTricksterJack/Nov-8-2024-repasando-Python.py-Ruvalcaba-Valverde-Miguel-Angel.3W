e42ddcd340e34aa2af660f15ed79cdf48bf2b134
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 20:16:32 -0600

    Update punto 12.py

--- punto 12.py
+++ punto 12.py
@@ -1,1 +1,7 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 11-----------")
+m1=["*","**","***","****","***** ","****","***","**","*",]#agamos una lista
+for x in m1:
+  print(x)#y que se repitan sus elementos hasta el fin
+print("--------------------------------------")
+print("Resultado: por fin, e terminado, se ven los dibujos.\n")
