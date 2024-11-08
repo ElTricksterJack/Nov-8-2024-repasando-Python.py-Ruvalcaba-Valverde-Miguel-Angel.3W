dff757ebc153aaa6070b0c7e1ecf374780373c1f
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-07 19:56:01 -0600

    Update punto 9.py

--- punto 9.py
+++ punto 9.py
@@ -1,1 +1,14 @@
-
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("-----------Actividad 9-----------")
+print("qui escribe 5 animes")#como dije hay que haser una aclarasion
+m1 = input("Anime 1:")#ahora bamos a capturar los datos
+m2 = input("Anime 2:")
+m3 = input("Anime 3:")
+m4 = input("Anime 4:")
+m5 = input("Anime 5:")
+anim=[m1,m2,m3,m4,m5]#los juntamos en una lista
+print("----------------")
+for x in anim:#con un for asemos un bucle
+  print(x)#y este es el dato que se repite, mostrar el dato hasta que acabe el bucle
+print("--------------------------------------")
+print("Resultado: se muestra los elementos de la lista.\n")
